64950e6c031f7d0ff039bc915dddb0d2370d0abd
AAA118-sys <63290467+AAA118-sys@users.noreply.github.com>
2022-04-19 10:53:19 +0300

    Add files via upload
    Реализация массива

---
+++ src/PM03/PM03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+public class Cheese : IComparable
+{
+    public string brand;
+    public string manufacturer;
+    public int fatPercentage;
+
+    public Cheese(string brand, string manufacturer, int fatPercentage)
+    {
+        this.brand = brand;
+        this.manufacturer = manufacturer;
+        this.fatPercentage = fatPercentage;
+    }
+
+    public string ToString()
+    {
+        return string.Format("Марка: {0}  Производитель: {1} Процент жирности: {2}", brand, manufacturer, fatPercentage);
+    }
+
+    
