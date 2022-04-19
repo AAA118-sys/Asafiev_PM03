73e9f0ac886c5e4d8b3a537e348a5baa1270ad25
AAA118-sys <63290467+AAA118-sys@users.noreply.github.com>
2022-04-19 11:56:33 +0300

    Итоговый файл

--- PM03/src/PM03/PM03/Program.cs
+++ PM03/src/PM03/PM03/Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
-namespace PM03
+public class Cheese : IComparable
 {
-    class Program
+    public string brand;
+    public string manufacturer;
+    public int fatPercentage;
+
+    public Cheese(string brand, string manufacturer, int fatPercentage)
     {
-        static void Main(string[] args)
+        this.brand = brand;
+        this.manufacturer = manufacturer;
+        this.fatPercentage = fatPercentage;
+    }
+
+    public string ToString()
+    {
+        return string.Format("Марка: {0}  Производитель: {1} Процент жирности: {2}", brand, manufacturer, fatPercentage);
+    }
+
+    public int CompareTo(object o)
+    {
+        Cheese c = o as Cheese;
+        if (c != null)
         {
-            char Cheese;
+            int result = manufacturer.CompareTo(c.manufacturer);
+            if (result != 0)
+            {
+                return result;
+            }
+            return fatPercentage.CompareTo(c.fatPercentage);
+        }
+        else
+        {
+            throw new Exception("Невозможно сравнить два объекта");
+        }
+    }
+
+}
+
+public class MilkFarm
+{
+    int cntProducts;
+    public Cheese[] Products;
+
+    public MilkFarm(int cntProducts)
+    {
+        this.cntProducts = cntProducts;
+        Products = new Cheese[cntProducts];
+    }
+
+    public void Fill()
+    {
+        string brand;
+        string manufacturer;
+        int fatPercentage;
+        for (int i = 0; i < this.cntProducts; i++)
+        {
+            Console.WriteLine("Марка:");
+            brand = Console.ReadLine();
+            Console.WriteLine("Производитель:");
+            manufacturer = Console.ReadLine();
+            Console.WriteLine("Процент жирности:");
+            fatPercentage = Convert.ToInt32(Console.ReadLine());
+            this.Products[i] = new Cheese(brand, manufacturer, fatPercentage);
+        }
+    }
+
+    public void Sort()
+    {
+        Array.Sort(this.Products);
+    }
+
+    public void PrintToFile()
+    {
+        using (StreamWriter file = new StreamWriter("result.txt", false, Encoding.UTF8))
+        {
+            foreach (Cheese c in this.Products)
+            {
+                file.WriteLine(c.ToString());
+            }
         }
     }
 }
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Количество продуктов:");
+        int cntProducts = Convert.ToInt32(Console.ReadLine());
+        MilkFarm milkFarm = new MilkFarm(cntProducts);
+        milkFarm.Fill();
+        milkFarm.Sort();
+        milkFarm.PrintToFile();
+    }
+}
+
