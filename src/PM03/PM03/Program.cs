fbf186967f362a4ab61d6eb879d5054e33861ad3
AAA118-sys <63290467+AAA118-sys@users.noreply.github.com>
2022-04-19 11:04:51 +0300

    Реализация сортировки

--- src/PM03/PM03/Program.cs
+++ src/PM03/PM03/Program.cs
@@ -20,4 +20,42 @@ public class Cheese : IComparable
         return string.Format("Марка: {0}  Производитель: {1} Процент жирности: {2}", brand, manufacturer, fatPercentage);
     }
 
-    
+    public int CompareTo(object o)
+    {
+        Cheese c = o as Cheese;
+        if (c != null)
+        {
+            int result = manufacturer.CompareTo(c.manufacturer);
+            if (result != 0)
+            {
+                return result;
+            }
+            return fatPercentage.CompareTo(c.fatPercentage);
+        }
+        else
+        {
+            throw new Exception("Невозможно сравнить два объекта");
+        }
+    }
+
+}
+
+
+
+    public void Sort()
+    {
+        Array.Sort(this.Products);
+    }
+
+    public void PrintToFile()
+    {
+        using (StreamWriter file = new StreamWriter("result.txt", false, Encoding.UTF8))
+        {
+            foreach (Cheese c in this.Products)
+            {
+                file.WriteLine(c.ToString());
+            }
+        }
+    }
+}
+
